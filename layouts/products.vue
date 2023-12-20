929e53975697c755c142dc8a376751d5c648fd9e
Cindy Chang <42115114+cindy951357@users.noreply.github.com>
2023-12-20 21:32:00 +0800

    add layout and tainwindcss

---
+++ layouts/products.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header>
+            <NuxtLink to="/products">Nuxt Dojo Merch</NuxtLink>
+        </header>
+    </div>
+    <div>
+        <slot></slot>
+    </div>
+    <footer>
+        <ul>
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/about">About</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/products">Products</NuxtLink>
+                </li>
+            </ul>
+    </footer>
+</template>
+<style scoped>
+.router-link-exact-active {
+    color: #12b488;
+}
+</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
-
+    definePageMeta({
+        layout: 'products'
+    });
 </script>
 
 <template>
