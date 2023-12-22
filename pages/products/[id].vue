b445ba7e95ba79058afcd9f44af342b0ce1251d6
Cindy Chang <42115114+cindy951357@users.noreply.github.com>
2023-12-22 14:51:04 +0800

    add useFetch

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,9 +1,16 @@
 <script lang="ts" setup>
 const { id } = useRoute().params;
+const uri = `https://fakestoreapi.com/products/${id}`
+
+const { data: product } = await useFetch(uri, {key: id});
+
 </script>
 
 <template>
     <div>Product details for {{ id }}</div>
+    <p>{{  product.title }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
 </template>
 
 <style scoped>
