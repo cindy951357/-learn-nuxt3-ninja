2a842f89cfd25585a90eb26ff7e2f8e109848af7
Cindy Chang <42115114+cindy951357@users.noreply.github.com>
2023-12-29 14:05:33 +0800

    user popup

--- components/Header.vue
+++ components/Header.vue
@@ -7,15 +7,25 @@
         </div>
         <div class="icon-group">
             <el-icon class="icon-share"><share /></el-icon>
-            <el-icon class="icon-user"><user /></el-icon>
+            <div class="popup-icon relative">
+                <el-icon class="icon-user" id="icon_user" @click="toggleUserPopup"><user /></el-icon>
+                <div v-if="isUserPopupOpen" class="popup-wrapper absolute"><UserPopup/></div>
+            </div>            
             <el-icon class="icon-service"><service /></el-icon>
-        </div>
+        </div>        
     </div>
+    
 </template>
 
 <script lang="ts" setup >
-import { defineComponent } from 'vue'
+import UserPopup from "@/components/popups/UserPopup.vue";
 import { Share, User, Service } from '@element-plus/icons-vue';
+import { ref } from 'vue';
+
+const isUserPopupOpen = ref(false);
+const toggleUserPopup = () => {
+    isUserPopupOpen.value = !isUserPopupOpen.value;
+}
 
 </script>
 
@@ -24,6 +34,7 @@ import { Share, User, Service } from '@element-plus/icons-vue';
     padding: 10px;
     width: 100%;
     display: flex;
+    position: relative;
 
     @media (min-width: 470px) {
         justify-content: space-between;
@@ -34,9 +45,18 @@ import { Share, User, Service } from '@element-plus/icons-vue';
 }
 .icon-group {
     color: white;
+    display: flex;
+    .el-icon:hover {
+        cursor: pointer;
+    }
     .el-icon svg{
         width: 30px;
         height: 30px;
     }
+}
+
+.popup-wrapper {
+    z-index: 1000;
+    left: calc(-400px + 30px);
 }
 </style>
